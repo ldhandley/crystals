6f9fd6d85f414d8e71136bbc07badecc69ab70a4
Lindsey Handley <lindseyhandley@gmail.com>
2021-02-11 18:57:12 +0000

    updated readme, github, pushing unreal stuff

--- README.md
+++ README.md
@@ -2,3 +2,32 @@ crystals
 ========
 `crystals` is a CodeSpells mod! Learn more at https://codespells.org.
 
+### Installation
+
+Clone this repo
+
+```
+cd crystals
+raco pkg install
+raco codespells-install
+```
+
+Open up `main.rkt` and adjust the `codespells-workspace` accordingly. Run.
+
+### Including this Rune Collection as a Package
+
+`(require-mod crystals)`
+
+This gives you access to `crystals:my-mod-lang` which you can combine into your mod's rune lang with `append-rune-langs`.
+
+### Unreal Development
+
+If you want to start up the Unreal project (`/Unreal`), install the following:
+* Real Rare Crystals Pack: https://www.unrealengine.com/marketplace/en-US/item/963c62c38abb403aa08b575584b765d0
+  * Place in: `Plugins/Crystals/Content/RealRareCrystals`
+
+### Releasing New Content
+
+If you have only changed the Racket code, just push to Github.
+
+If you have changed or added Unreal assets, push to Github and run `raco codespells-release`.
--- main.rkt
+++ main.rkt
@@ -5,22 +5,22 @@
 (define-classic-rune (orange-crystal)
   #:background "blue"
   #:foreground (circle 40 'solid 'orange)
-  (spawn-mod-blueprint pak-folder mod-name "OrangeCrystal"))
+  (with-scale 5 (spawn-mod-blueprint pak-folder mod-name "OrangeCrystal")))
 
 (define-classic-rune (green-crystal)
   #:background "blue"
   #:foreground (circle 40 'solid 'green)
-  (spawn-mod-blueprint pak-folder mod-name "GreenCrystal"))
+  (with-scale 5 (spawn-mod-blueprint pak-folder mod-name "GreenCrystal")))
 
 (define-classic-rune (blue-crystal)
   #:background "blue"
   #:foreground (circle 40 'solid 'blue)
-  (spawn-mod-blueprint pak-folder mod-name "BlueCrystal"))
+  (with-scale 5 (spawn-mod-blueprint pak-folder mod-name "BlueCrystal")))
 
 (define-classic-rune (dark-crystal)
   #:background "blue"
   #:foreground (circle 40 'solid 'purple)
-  (spawn-mod-blueprint pak-folder mod-name "DarkCrystal"))
+  (with-scale 5 (spawn-mod-blueprint pak-folder mod-name "DarkCrystal")))
 
 (define-classic-rune-lang my-mod-lang #:eval-from main.rkt
   (orange-crystal green-crystal blue-crystal dark-crystal))
@@ -28,7 +28,8 @@
 (module+ main
   (codespells-workspace ;TODO: Change this to your local workspace if different
    (build-path (current-directory) ".." ".."))
-  
+
+
   (once-upon-a-time
    #:world (arena-world)
    #:aether (demo-aether
